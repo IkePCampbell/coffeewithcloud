65ed6a13e5dad8c107d5fa115832aa95a8d59f46
IkePCampbell <isaacpcampbell98@gmail.com>
2020-09-03 14:10:39 -0500

    Added skeleton

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 import {blogs} from '../../data/blogs'
+
 
 const Blogs = ({blogs}) => {
     return blogs.reverse().map((b, index) => {
@@ -20,9 +20,20 @@ export default function Home () {
 
     return (
         <>
-            <header>
-                hi welcome to my blog
+            <header class="homeheader">
+                CoffeeCloud
             </header>
+            <header class="homeheadersub">Exploring newest technologies, one cup at a time</header>
+            <table class = "aboutMe" align="center">
+                <tr>
+                    <td align = "left" valign = "top"><img src={require('./profilepic.png')} class="profile"/></td>
+                    <td align = "left" valign = "top"><p><br></br>Welcome to the blog! Grab a seat your coffee is almost ready!<br></br>While you wait, check out <a href="http://www.github.com/ikepcampbell">my GitHub</a> for all content in this blog.</p></td>
+                </tr>
+                
+                
+            </table>
+            
+
             <main>
                 <Blogs blogs={blogs} />
             </main>
--- src/data/blogs.js
+++ src/data/blogs.js
@@ -1,16 +1,10 @@
 export const blogs = [
+
     {
-        name: "blog 1",
-        date: "01-01-2020",
-        author: "firstname lastname",
-        teaser: "this article is about cats",
-        text: "cat stuff cat stuff cat stuff ..."
-    },
-    {
-        name: "blog 2",
-        date: "02-02-2020",
-        author: "firstname lastname",
-        teaser: "this article is about dogs",
+        name: "''Hello World''",
+        date: "09-02-2020",
+        author: "Isaac Campbell",
+        teaser: "Origin Story",
         text: "dog stuff dog stuff dog stuff ..."
     }
 ]
