5e2759503e56cfd6e9c958c6a27a0a0e2d3e3a5b
nicklaslanman <nicklasllanman@gmail.com>
2020-09-21 17:23:58 -0500

    minor styles, console cleanup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,24 +5,24 @@ import cloud from '../../img/coffeecloud.png'
 const Header = () => {
     return (
         <>
-        <nav class="navbar navbar-expand-lg navbar-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark">
             <Link to={`/`} className="nav-brand"><img src={cloud} alt="coffeecloud"/></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item">
                     <Link to={`/`} className = "nav-link">Home</Link>
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                     <Link to={`/blogs`} className = "nav-link">Search for Blogs</Link>
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                     <Link to={`/contactme`} className = "nav-link">Got Questions?</Link>
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                     <Link to={`/about`} className = "nav-link">About Me</Link>
                 </li>
             </ul>
